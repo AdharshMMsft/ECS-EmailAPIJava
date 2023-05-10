1e5fb37256dd1e9d2f5542df76bdfa89a82e05cc
Adharsh M <adharshm@microsoft.com>
2023-05-10 13:38:12 +0530

    Added Email API code

---
+++ EmailGA/src/main/java/Email.java
@@ -0,0 +1,37 @@
+import java.lang.Object;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.core.util.Context;
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.communication.*;
+import com.azure.resourcemanager.communication.CommunicationManager;
+import com.azure.resourcemanager.communication.models.EmailServiceResource;
+
+
+
+import java.lang.*;
+
+public class Email {
+
+    public static void main(String[] args) {
+        try {
+            AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE); // checking the environment
+            TokenCredential credential = new DefaultAzureCredentialBuilder().authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint()).build(); // building the credential
+            CommunicationManager manager = CommunicationManager.authenticate(credential, profile); // can create a comms manager.
+            PagedIterable <EmailServiceResource>  emailServiceResources = manager.emailServices().list(Context.NONE); // make a PagedIterable container with email services listed.
+            for (EmailServiceResource x : emailServiceResources) {
+                System.out.println(x.name().toString());    // Need to print the name using toString().
+            }
+
+        }
+        catch (Exception e) {
+            System.out.println(e.toString());   // Read with the exception.
+        }
+
+    }
+}
